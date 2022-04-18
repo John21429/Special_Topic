6e38205960a6ee6680b106ffe4038b6f2935088f
john21429 <john21429@gmail.com>
2022-04-18 23:49:17 +0800

    f7

--- javescript/index.js
+++ javescript/index.js
@@ -1,32 +1,28 @@
 
 // var icon =document.getElementsByClassName("fa-solid");
 document.addEventListener('scroll', function(e) {
-
     if(window.scrollY >= 700){
- 
         //要做的事情放這邊
         document.getElementsByClassName("logo")[0].src ="../img/LOGO_F2_B.png";
         var icon =document.getElementsByClassName("fa-solid");
-
         for(let i = 0; i< icon.length; i++){
             // console.log(icon[i]);
             icon[i].style.color = "black";
         }
-        
-       }else{
+        }else{
         document.getElementsByClassName("logo")[0].src ="../img/LOGO_F2W_1.png";
         var icon =document.getElementsByClassName("fa-solid");
         for(let i = 0; i< icon.length; i++){
             // console.log(icon[i]);
             icon[i].style.color = "white";
-       }}        
-      });
+        }
+    }        
+});
+
 var b4h2_at_high = 1200;//設定高度
 
 document.addEventListener('scroll', function() {
-
     if(window.scrollY >= b4h2_at_high){
-        
         //要做的事情放這邊
     document.getElementsByClassName("b4tb_h2")[0].classList.add("b4h2_at");
     }           
